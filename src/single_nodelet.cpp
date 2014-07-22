a027e105e86c46f2bd0b848c4cb3b2f6e279296f
Chao Qu <quchao@seas.upenn.edu>
2014-07-22 12:21:22 -0400

    working single nodelet

---
+++ src/single_nodelet.cpp
@@ -0,0 +1,27 @@
+#include "bluefox2/single_camera.h"
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+
+
+namespace bluefox2 {
+
+class SingleNodelet : public nodelet::Nodelet {
+ public:
+  SingleNodelet() : nodelet::Nodelet() {}
+  ~SingleNodelet() { single_camera_->End(); }
+
+  virtual void onInit() {
+    single_camera_.reset(new SingleCamera(getPrivateNodeHandle()));
+    single_camera_->Run();
+  }
+
+ private:
+  std::unique_ptr<SingleCamera> single_camera_;
+};
+
+PLUGINLIB_DECLARE_CLASS(bluefox2, SingleNodelet, bluefox2::SingleNodelet,
+                        nodelet::Nodelet)
+
+}  // namespace bluefox2
