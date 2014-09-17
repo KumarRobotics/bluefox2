c61350213576dc1f917f1081ed4f7d5f4d1632a2
Chao Qu <quchao@seas.upenn.edu>
2014-09-17 19:58:39 -0400

    udpate stereo calib

--- src/bluefox2_ros.cpp
+++ src/bluefox2_ros.cpp
@@ -5,7 +5,7 @@ namespace bluefox2 {
 bool Bluefox2Ros::Grab(const sensor_msgs::ImagePtr &image_msg,
                        const sensor_msgs::CameraInfoPtr &cinfo_msg) {
   // Add expose time to current time stamp
-  image_msg->header.stamp += ros::Duration(bluefox2_.expose_us() * 1e-6);
+  //  image_msg->header.stamp += ros::Duration(bluefox2_.expose_us() * 1e-6);
   return bluefox2_.GrabImage(*image_msg);
 }
 
